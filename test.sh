f74c19edb407c5e083c801fc1016b89bd04be84c
Beilong Tang <bt132@duke.edu>
2024-10-02 15:26:19 +0800

    update

--- test.sh
+++ test.sh
@@ -4,4 +4,16 @@
 #SBATCH -N 1
 #SBATCH --gres=dcu:4
 #SBATCH -p kshdnormal02
+
+export ROCBLAS_TENSILE_LIBPATH=/public/software/compiler/rocm/dtk-23.10/lib/rocblas/library_dcu2
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate bltang_new
+
+module purge
+module load compiler/devtoolset/7.3.1
+module load mpi/hpcx/2.7.4/gcc-7.3.1
+module load compiler/rocm/dtk-23.10
+
+
 python test.py
