472cb91d2b8335e3c0227889bf6bf607d72b9285
Beilong Tang <bt132@duke.edu>
2024-10-02 15:49:36 +0800

    update

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@
 #SBATCH -N 1
 #SBATCH --gres=dcu:4
 #SBATCH -p kshdnormal02
-#SBATCH -o log.out
 #SBATCH -e log.err
 
 export ROCBLAS_TENSILE_LIBPATH=/public/software/compiler/rocm/dtk-23.10/lib/rocblas/library_dcu2
@@ -18,4 +17,4 @@ module load mpi/hpcx/2.7.4/gcc-7.3.1
 module load compiler/rocm/dtk-23.10
 
 
-python -u test.py
+python -u test.py > log.out
