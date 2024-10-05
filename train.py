41b45538d02019ef038137e01b3a6b483c6a9147
Beilong Tang <bt132@duke.edu>
2024-10-05 14:55:39 +0800

    update

--- train.py
+++ train.py
@@ -96,7 +96,8 @@ def main(rank, args):
     data = train_iter.build_iter(0)
     for i, (uttid, data) in enumerate(data):
         l.info(data)
-        torch.save(data, "data.pt")
+        if rank == 0:
+            torch.save(data, "data.pt")
         break
 
     ct = 0
