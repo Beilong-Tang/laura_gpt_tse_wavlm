12b65abc966f81a2baa2a8dd6489645e7b9c0e54
Beilong Tang <bt132@duke.edu>
2024-10-05 15:10:43 +0800

    update

--- test.sh
+++ test.sh
@@ -18,7 +18,6 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate bltang_new
 
 module purge
-module purge
 module load compiler/devtoolset/7.3.1
 module load mpi/hpcx/2.7.4/gcc-7.3.1
 module load compiler/rocm/dtk-23.04.1
