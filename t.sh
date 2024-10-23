0a899f8842a5ee8b44b2bfd183f45c55f110e474
Beilong-Tang <bt132@duke.edu>
2024-10-23 16:34:09 +0800

    update

---
+++ t.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J testing_framework
+#SBATCH -n 32
+#SBATCH -N 1
+#SBATCH --gres=dcu:4
+#SBATCH -p kshdnormal
+#SBATCH -o log.out
+#SBATCH -e log.err
+
+
+export ROCBLAS_TENSILE_LIBPATH=/public/software/compiler/rocm/dtk-23.10/lib/rocblas/library_dcu2
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate bltang_new
+
+module purge
+module load compiler/devtoolset/7.3.1
+module load mpi/hpcx/2.7.4/gcc-7.3.1
+module load compiler/rocm/dtk-23.10
+
+
+python -u t.py
