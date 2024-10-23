a55e338d0f6b3c2d3a2a3eb44ffa8ac2a28e85c9
Beilong-Tang <bt132@duke.edu>
2024-10-23 15:04:07 +0800

    update

---
+++ train.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -J train_laura_se
+#SBATCH -n 32
+#SBATCH -N 1
+#SBATCH --gres=dcu:4
+#SBATCH -p kshdnormal
+#SBATCH -o log.out
+#SBATCH -e log.err
+
+export MIOPEN_FIND_MODE=3
+export HSA_FORCE_FINE_GRAIN_PRICE=1
+export NCCL_IB_HCA=mlx5_0
+export NCCL_SOCKET_IFNAME=ib0
+
+# export ROCBLAS_TENSILE_LIBPATH=/public/software/compiler/rocm/dtk-23.10/lib/rocblas/library_dcu2
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate bltang_new
+
+module purge
+module load compiler/devtoolset/7.3.1
+module load mpi/hpcx/2.7.4/gcc-7.3.1
+module load compiler/rocm/dtk-23.04.1
+
+
+python -u train.py --config exp/se_wavlm/config/conf.yaml
