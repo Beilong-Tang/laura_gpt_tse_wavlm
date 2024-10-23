5d2c031c27e922151ae66356e1240c14d84a4e09
Beilong-Tang <bt132@duke.edu>
2024-10-23 16:15:59 +0800

    update

--- train.sh
+++ train.sh
@@ -23,4 +23,4 @@ module load mpi/hpcx/2.7.4/gcc-7.3.1
 module load compiler/rocm/dtk-23.10
 
 
-srun python -u train.py --config exp/se_wavlm/config/conf.yaml
+python -u train.py --config exp/se_wavlm/config/conf.yaml
