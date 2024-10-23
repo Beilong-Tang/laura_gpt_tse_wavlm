0e23386cb44a2d27766fe217484da4ad398c9d3b
Beilong-Tang <bt132@duke.edu>
2024-10-23 16:11:16 +0800

    update

--- train.sh
+++ train.sh
@@ -7,12 +7,12 @@
 #SBATCH -o log.out
 #SBATCH -e log.err
 
-export MIOPEN_FIND_MODE=3
-export HSA_FORCE_FINE_GRAIN_PRICE=1
-export NCCL_IB_HCA=mlx5_0
-export NCCL_SOCKET_IFNAME=ib0
+# export MIOPEN_FIND_MODE=3
+# export HSA_FORCE_FINE_GRAIN_PRICE=1
+# export NCCL_IB_HCA=mlx5_0
+# export NCCL_SOCKET_IFNAME=ib0
 
-# export ROCBLAS_TENSILE_LIBPATH=/public/software/compiler/rocm/dtk-23.10/lib/rocblas/library_dcu2
+export ROCBLAS_TENSILE_LIBPATH=/public/software/compiler/rocm/dtk-23.10/lib/rocblas/library_dcu2
 
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate bltang_new
@@ -20,7 +20,7 @@ conda activate bltang_new
 module purge
 module load compiler/devtoolset/7.3.1
 module load mpi/hpcx/2.7.4/gcc-7.3.1
-module load compiler/rocm/dtk-23.04.1
+module load compiler/rocm/dtk-23.10
 
 
 python -u train.py --config exp/se_wavlm/config/conf.yaml
