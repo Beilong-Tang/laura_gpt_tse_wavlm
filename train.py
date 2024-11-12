45c829cf0ace610a85e49de359acd9b7d3fd49a7
Beilong <bt132@duke.edu>
2024-11-12 13:04:03 +0800

    update

--- train.py
+++ train.py
@@ -49,7 +49,8 @@ def main(rank, args):
     l.info(args)
     l.info(f"rank {rank} of world_size {len(args.gpus)} started...")
     setup(rank, len(args.gpus), args.dist_backend)
-    args.gpu = args.gpus[rank]
+    # args.gpu = args.gpus[rank]
+    args.gpu = rank
     print(f"gpu id: {args.gpu}")
     torch.cuda.set_device(args.gpu)
     torch.cuda.empty_cache()
@@ -103,7 +104,7 @@ if __name__ == "__main__":
     update_args(args,args.config)
     log = setup_logger(args, rank = 0)
     log.info(f"torch cuda available: {torch.cuda.is_available()}")
-    # os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
     args.gpus = [int(i) for i in args.gpus.split(",")]
     args.ngpu = len(args.gpus)
     mp.spawn(main, args=(args,), nprocs=len(args.gpus), join=True)
