a84a0830901fd9e47ce12fdc6d134aee503648f0
Beilong <bt132@duke.edu>
2024-11-14 09:35:08 +0800

    update

--- inference/infer_tse.sh
+++ inference/infer_tse.sh
@@ -1,4 +1,4 @@
-python -u inference/infer.py --config_file exp/tse_wavlm/config/conf.yaml --model_file /DKUdata/tangbl/laura_gpt_tse/exp/tse_wavlm/ckpt/conf/best.pth --output_dir /DKUdata/tangbl/laura_gpt_tse/output/conf --default_config inference/infer_no_ref_decoder.yaml --device cuda:4
+python -u inference/infer.py --config_file exp/tse_wavlm/config/conf.yaml --model_file /DKUdata/tangbl/laura_gpt_tse/exp/tse_wavlm/ckpt/conf/best.pth --output_dir /DKUdata/tangbl/laura_gpt_tse/output/conf --default_config inference/infer_no_ref_decoder.yaml --device cuda:2
 
 ## Infer with auxilary audio
 python -u inference/infer.py --config_file exp/tse_wavlm/config/conf.yaml --model_file /DKUdata/tangbl/laura_gpt_tse/exp/tse_wavlm/ckpt/conf/best.pth --output_dir /DKUdata/tangbl/laura_gpt_tse/output/conf --default_config inference/infer.yaml --device cuda:2
