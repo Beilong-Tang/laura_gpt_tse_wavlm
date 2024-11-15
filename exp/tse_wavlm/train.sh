bf752e0f2eaaa79875382db6e44b121cbe48a780
Beilong <bt132@duke.edu>
2024-11-15 14:41:26 +0800

    update

--- exp/tse_wavlm/train.sh
+++ exp/tse_wavlm/train.sh
@@ -1,1 +1,4 @@
 python -u train.py --config exp/tse_wavlm/config/conf.yaml --gpus 6,7 --log ./log/tse_wavlm
+
+## Train using librispeech
+python -u train.py --config exp/tse_wavlm/config/conf_ljspeech.yaml --gpus 6,7 --log ./log/tse_wavlm
